be7bc857897bc13f7266712c9fb8320c4435ad20
Gerardo Sanchez <oracle942@gmail.com>
2021-08-22 21:30:49 -0700

    Completed row duplication process.

--- script.js
+++ script.js
@@ -1,20 +1,36 @@
   
 var today = moment();
 var currentDay = $('#currentDay')
-var textInput1 = document.getElementById(".text-input1")
-var save1Btn = $('.saveBtn')
-var localText1 = localStorage.getItem("storedText1")
-
-$('.text-input1').attr('placeholder', localText1);
-// This is meant to add the local storage item via textContent.
-// This is the intended method, but error occurs for unknown reasons.
-// $('.text-input1').attr('textContent', localText1);
-
-console.log(localText1)
 $(currentDay).text(today.format("MMM Do, YYYY"));
 
-save1Btn.on("click", function(){ 
-    var savedText1 = $('.text-input1')
-    localStorage.setItem("storedText1", savedText1.val());
+
+var saveBtn = $('.saveBtn')
+var localText = localStorage.getItem("storedText")
+
+$('.text-input').attr('placeholder', localText);
+// This is meant to add the local storage item via textContent.
+// This is the intended method, but error occurs for unknown reasons.
+// If functional, this line of code would repeat
+// $('.text-input1').attr('textContent', localText1);
+
+saveBtn.on("click", function(){ 
+    var savedText = $('.text-input')
+    localStorage.setItem("storedText", savedText.val());
+    
+});
+
+
+
+
+
+
+var saveBtn2 = $('.saveBtn2')
+var localText2 = localStorage.getItem("storedText2")
+
+$('.text-input2').attr('placeholder', localText2);
+saveBtn2.on("click", function(){ 
+    var savedText2 = $('.text-input2')
+    localStorage.setItem("storedText2", savedText2.val());
    
 });
+
