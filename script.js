3c6749f7ebd1422e2d7ac27f087595a8e7b0d66b
Gerardo Sanchez <oracle942@gmail.com>
2021-08-22 20:42:51 -0700

    Began adding rows.

--- script.js
+++ script.js
@@ -1,13 +1,16 @@
+  
 var today = moment();
 var currentDay = $('#currentDay')
 var textInput1 = document.getElementById(".text-input1")
 var save1Btn = $('.saveBtn')
 var localText1 = localStorage.getItem("storedText1")
 
-// $('.text-input1').attr('placeholder', localText1);
-$('.text-input1').attr('textContent', localText1);
+$('.text-input1').attr('placeholder', localText1);
+// This is meant to add the local storage item via textContent.
+// This is the intended method, but error occurs for unknown reasons.
+// $('.text-input1').attr('textContent', localText1);
 
-console.log(textInput1)
+console.log(localText1)
 $(currentDay).text(today.format("MMM Do, YYYY"));
 
 save1Btn.on("click", function(){ 
@@ -15,6 +18,3 @@ save1Btn.on("click", function(){
     localStorage.setItem("storedText1", savedText1.val());
    
 });
-
-
-// console.log($('.text-input1').text())
