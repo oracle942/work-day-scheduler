5c499ff3680cf1dd44806b389933c2802456ea28
Gerardo Sanchez <oracle942@gmail.com>
2021-08-22 17:19:49 -0700

    Styling added to rows. Added local storage functionality.

--- script.js
+++ script.js
@@ -1,9 +1,19 @@
 var today = moment();
 var currentDay = $('#currentDay')
-var text = localStorage.getItem("text")
+var textInput1 = $('.text-input1').text()
+var save1Btn = $('.saveBtn')
+var localText1 = localStorage.getItem("storedText1")
+var storedText1
+
+textInput1 = localText1
 
 $(currentDay).text(today.format("MMM Do, YYYY"));
 
+save1Btn.on("click", function(){ 
+    var storedText1 = $('.text-input1').text()
+    localStorage.setItem("storedText1", storedText1);
+    console.log(storedText1)
+});
 
 
-
+console.log($('.text-input1').text())
